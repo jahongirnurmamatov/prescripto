993a77e5a564356a50ce8e5ca2291b721e3673a4
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-10-21 23:12:07 +0500

    contact page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <div className="text-center text-2xl pt-10 text-gray-500">
+        <p>CONTACT <span className='text-gray-700 font-semibold'>US</span></p>
+      </div>  
+      <div className="my-1 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm">
+        <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-lg text-gray-600'>OUR OFFICE</p>
+          <p className='text-gray-500'>GALABA 216 - 60 <br />NAVOI, UZBEKISTAN</p>
+          <p className='text-gray-500'>Tel: +998999404612 <br />Email:jahongiir.nurmamatov@gmail.com</p>
+          <p className='font-semibold text-gray-600'>Careers at PRESCRIPTO</p>
+          <p className='text-gray-500'>Learn more about our teams and job openings</p>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
