18b04e0689158ca42999c3f0c57840c070d6a7dd
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-10-23 21:38:45 +0500

    login user api

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -36,4 +36,25 @@ const registerUser = async (req, res) => {
   }
 };
 
-export { registerUser };
+const loginUser = async (req, res) => {
+  try {
+    const {email,password} = req.body;
+    const user = await userModel.findOne({email});
+
+    if(!user){
+        return res.json({success:false,message:'User not registered, please register first!'});
+    }
+    const isMatch = await bcrypt.compare(password,user.password);
+    if(isMatch){
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET);
+        res.json({success:true,token});
+    }else{
+        return res.json({success:false,message:'Invalid credentials1'});
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { registerUser,loginUser };
