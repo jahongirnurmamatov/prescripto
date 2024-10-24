856a60c852e5e4ceb7b50f666dd8d3c32a62e170
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-10-24 13:03:44 +0500

    listAppointment api

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -149,4 +149,15 @@ const bookAppointment = async(req,res)=>{
   }
 }
 
+// api to get user appointments for frontend
+const listAppointment = async(req,res)=>{
+  try {
+    const {userId} = req.body;
+    const appointments = await appointmentModel.find({userId});
+    res.json({success:true, appointments});
+  } catch (error) {
+    console.log(error);
+    res.json({success:false, message:error.message});
+  }
+}
 export { registerUser, loginUser, getProfile, updateProfile,bookAppointment };
